41a6ace4d84f53b6ccd1a42d6de1dda252a1ed0f
Mek <phongpak50@gmail.com>
2024-04-22 19:29:53 +0700

    commit ปรับแก้ UI เพิ่มข้อมูลคอนเทนต์

---
+++ centersdk/src/main/java/com/starvision/view/center/SvLogoutConfirmDialogFragment.kt
@@ -0,0 +1,21 @@
+package com.starvision.view.center
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class SvLogoutConfirmDialogFragment:DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
