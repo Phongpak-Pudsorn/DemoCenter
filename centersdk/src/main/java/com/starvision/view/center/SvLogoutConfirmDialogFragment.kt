ab90d8e9e4d84b607591616751efa73b80d4e99d
Mek <phongpak50@gmail.com>
2024-04-23 09:54:13 +0700

    commit อัพเดท logout confirm

--- centersdk/src/main/java/com/starvision/view/center/SvLogoutConfirmDialogFragment.kt
+++ centersdk/src/main/java/com/starvision/view/center/SvLogoutConfirmDialogFragment.kt
@@ -5,8 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import com.starvision.luckygamesdk.R
+import com.starvision.luckygamesdk.databinding.PageLogoutConfirmBinding
 
 class SvLogoutConfirmDialogFragment:DialogFragment() {
+    val binding: PageLogoutConfirmBinding by lazy { PageLogoutConfirmBinding.inflate(layoutInflater) }
+    private lateinit var mConfirmListener : ConfirmListener
+    interface ConfirmListener {
+        fun onLogout()
+        fun onCancel()
+    }
+    fun setClickListener(listener : ConfirmListener) {
+        mConfirmListener = listener
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,5 +28,18 @@ class SvLogoutConfirmDialogFragment:DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val width = ViewGroup.LayoutParams.MATCH_PARENT
+        dialog!!.window!!.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog!!.window!!.setBackgroundDrawableResource(R.color.transparent)
+        dialog!!.show()
+        dialog!!.setOnCancelListener {
+            mConfirmListener.onCancel()
+        }
+        binding.btnYes.setOnClickListener {
+            dialog!!.dismiss()
+        }
+        binding.btnNo.setOnClickListener {
+            dialog!!.dismiss()
+        }
     }
 }
